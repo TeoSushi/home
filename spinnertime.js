fbaf03371fa504bbb302c812cfe9a1f465b8ab16
unknown <teosushi1014@gmail.com>
2023-12-14 22:14:21 +0700

    Add video lny.mp4

--- spinnertime.js
+++ spinnertime.js
@@ -1,15 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Hiển thị spinner và overlay khi trang web được tải
     document.getElementById("spinner-container").style.display = "block";
     document.getElementById("transparent-overlay").style.display = "block";
-    document.body.classList.add("transparent-effect"); // Thêm hiệu ứng transparent
+    document.body.classList.add("transparent-effect");
 
-    // Ẩn spinner và overlay sau 1 giây
     setTimeout(function () {
-        // Gỡ bỏ lớp transparent-effect trước khi ẩn spinner và overlay
         document.body.classList.remove("transparent-effect");
 
-        // Ẩn spinner và overlay sau một khoảng thời gian nhỏ
         setTimeout(function () {
             document.getElementById("spinner-container").style.display = "none";
             document.getElementById("transparent-overlay").style.display = "none";
